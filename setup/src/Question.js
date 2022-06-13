2c93e7e1d5e0004ca039a1e2a9c94ad0e6e23517
Nelson Guerra <nelsonguerra.tech@outlook.com>
2022-06-13 12:21:20 -0400

    Accordion completed

--- setup/src/Question.js
+++ setup/src/Question.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ title, info }) => {
+  // statr value for the button
+  const [showInfo, setShowInfo] = useState(false);
+
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => setShowInfo(!showInfo)}>
+          {/* ternary operator for the icons */}
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
